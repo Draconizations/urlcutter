d6e5a6f2f55a49eba6fcb5c865666fb3f895db14
Jake Fulmine <jake@fulmine.xyz>
2023-11-07 09:40:40 +0100

    fix default date value

--- migrations/0000_greedy_dark_beast.sql
+++ migrations/0000_greedy_dark_beast.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `long_url` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`short_url_id` integer,
-	`created` integer DEFAULT 2023-11-06T18:54:07.035Z,
+	`created` integer NOT NULL,
 	`version_tag` text NOT NULL,
 	`long_url` text NOT NULL,
 	FOREIGN KEY (`short_url_id`) REFERENCES `short_url`(`id`) ON UPDATE no action ON DELETE no action
@@ -9,7 +9,7 @@ CREATE TABLE `long_url` (
 --> statement-breakpoint
 CREATE TABLE `short_url` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`created` integer DEFAULT 2023-11-06T18:54:07.035Z,
+	`created` integer NOT NULL,
 	`is_public` integer NOT NULL,
 	`short_url` text NOT NULL
 );
--- src/data/schema.ts
+++ src/data/schema.ts
@@ -1,8 +1,9 @@
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core"
+import { sql } from "drizzle-orm"
 
 export const url = sqliteTable("short_url", {
 	id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
-	created: integer("created", { mode: "timestamp" }).default(new Date()),
+	created: integer("created", { mode: "timestamp" }).notNull(),
 	isPublic: integer("is_public", { mode: "boolean" }).notNull(),
 	shortUrl: text("short_url").notNull()
 })
@@ -10,7 +11,7 @@ export const url = sqliteTable("short_url", {
 export const longUrl = sqliteTable("long_url", {
 	id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
 	shortUrlId: integer("short_url_id").references(() => url.id),
-	created: integer("created", { mode: "timestamp" }).default(new Date()),
+	created: integer("created", { mode: "timestamp" }).notNull(),
 	versionTag: text("version_tag").notNull(),
 	longUrl: text("long_url").notNull()
 })
