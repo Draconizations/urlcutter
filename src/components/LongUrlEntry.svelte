b64d58f45a260101a0e4487a0b900499457733b5
Jake Fulmine <jake@fulmine.xyz>
2023-11-08 19:56:04 +0100

    add copy link button

--- src/components/LongUrlEntry.svelte
+++ src/components/LongUrlEntry.svelte
@@ -1,35 +1,47 @@
 <script lang="ts">
 	import { enhance } from "$app/forms"
+	import { page } from "$app/stores"
 	import type { LongUrl } from "$lib/types"
 
 	export let longUrl: LongUrl
 	export let shortUrl: string
 	export let env: "admin" | "public" = "public"
+
+	function copyUrl(shortUrl: string, versionTag: string) {
+		if (navigator) {
+			navigator.clipboard.writeText(`https://${$page.url.host}/${shortUrl}/${versionTag}`)
+		}
+	}
 </script>
 
 <li class="block col">
-	<div class="row-wrap" style="justify-content: space-between; align-items: center;">
-		<span style="flex: 1;">
-			<b><a href={`/${shortUrl}/${longUrl.versionTag}`}>{longUrl.versionTag}</a></b>
-		</span>
-		<span>
-			{longUrl.created
-				.toLocaleString("en-US", {
-					day: "2-digit",
-					month: "short",
-					hour: "2-digit",
-					hourCycle: "h24",
-					minute: "2-digit"
-				})
-				.toLowerCase()}
-			{#if env === "admin"}
-				<form style="display: inline;" action="?/deleteLongUrl" method="post" use:enhance>
-					<input type="hidden" name="short-url" value={shortUrl} />
-					<input type="hidden" name="version-tag" value={longUrl.versionTag} />
-					<input type="submit" style="margin: 0;" class="bg-failure" value="Delete" />
-				</form>
-			{/if}
-		</span>
+	<div class="row-wrap">
+		<button class="bg-success" style="margin-right: 0.5rem;" on:click={() => copyUrl(shortUrl, longUrl.versionTag)}>
+			copy
+		</button>
+		<div class="row-small">
+			<span style="flex: 1;">
+				<b><a href={`/${shortUrl}/${longUrl.versionTag}`}>{longUrl.versionTag}</a></b>
+			</span>
+			<span>
+				{longUrl.created
+					.toLocaleString("en-US", {
+						day: "2-digit",
+						month: "short",
+						hour: "2-digit",
+						hourCycle: "h24",
+						minute: "2-digit"
+					})
+					.toLowerCase()}
+			</span>
+		</div>
+		{#if env === "admin"}
+			<form style="display: flex; flex-direction: column;" action="?/deleteLongUrl" method="post" use:enhance>
+				<input type="hidden" name="short-url" value={shortUrl} />
+				<input type="hidden" name="version-tag" value={longUrl.versionTag} />
+				<input type="submit" style="margin: 0; flex: 1;" class="bg-failure" value="Delete" />
+			</form>
+		{/if}
 	</div>
 	<hr />
 	<details>
@@ -41,3 +53,20 @@
 		</div>
 	</details>
 </li>
+
+<style>
+	.row-small {
+		flex: 1;
+		display: flex;
+		flex-wrap: wrap;
+		flex-direction: column;
+		align-items: flex-end;
+	}
+
+	@media (min-width: 420px) {
+		.row-small {
+			flex-direction: row;
+			align-items: center;
+		}
+	}
+</style>
--- src/components/ShortUrlEntry.svelte
+++ src/components/ShortUrlEntry.svelte
@@ -1,32 +1,59 @@
 <script lang="ts">
 	import type { ShortUrl } from "$lib/types"
+	import { page } from "$app/stores"
 
 	export let url: ShortUrl
 	export let env: "admin" | "public" = "public"
+
+	function copyUrl(shortUrl: string) {
+		if (navigator) {
+			navigator.clipboard.writeText(`https://${$page.url.host}/${shortUrl}`)
+		}
+	}
 </script>
 
 <li class="block col">
-	<div class="row-wrap" style="justify-content: space-between">
-		<span style="flex: 1;">
-			<b><a href={`/${url.shortUrl}`}>{url.shortUrl}</a></b>
-			{#if env === "admin" && url.isPublic}
-				(public)
-			{/if}
-		</span>
-		<span>
-			{url.created
-				.toLocaleString("en-US", {
-					day: "2-digit",
-					month: "short",
-					hour: "2-digit",
-					hourCycle: "h24",
-					minute: "2-digit"
-				})
-				.toLowerCase()}
-			{#if env === "admin"}
-				✦
-				<a href={`/admin/${url.shortUrl}`}>view</a>
-			{/if}
-		</span>
+	<div class="row-wrap">
+		<button class="bg-success" on:click={() => copyUrl(url.shortUrl)}>copy</button>
+		<div class="row-small">
+			<span style="flex: 1;">
+				<b><a href={`/${url.shortUrl}`}>{url.shortUrl}</a></b>
+				{#if env === "admin" && url.isPublic}
+					(public)
+				{/if}
+			</span>
+			<span>
+				{url.created
+					.toLocaleString("en-US", {
+						day: "2-digit",
+						month: "short",
+						hour: "2-digit",
+						hourCycle: "h24",
+						minute: "2-digit"
+					})
+					.toLowerCase()}
+				{#if env === "admin"}
+					✦
+					<a href={`/admin/${url.shortUrl}`}>view</a>
+				{/if}
+			</span>
+		</div>
 	</div>
 </li>
+
+<style>
+	.row-small {
+		flex: 1;
+		display: flex;
+		flex-wrap: wrap;
+		flex-direction: column;
+		align-items: flex-end;
+	}
+
+	@media (min-width: 420px) {
+		.row-small {
+			flex-direction: row;
+			align-items: center;
+		}
+	}
+</style>
