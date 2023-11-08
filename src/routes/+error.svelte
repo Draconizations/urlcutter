c9051bb8b9778f2a5721e656f99f10a79c6be81e
Jake Fulmine <jake@fulmine.xyz>
2023-11-08 16:08:03 +0100

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { page } from "$app/stores"
+</script>
+
+<section class="section-center col" style="gap: 1rem; justify-content: center;">
+	<h2>
+		❌
+		<br /><br />
+		{$page.status}: {$page.error?.message}
+	</h2>
+</section>
+
+<svelte:head>
+	<title>URLCutter | Error</title>
+</svelte:head>
